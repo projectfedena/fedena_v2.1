bb80b51af6dfd6ab33b9d3f27d5acf2f07d26fc1
foradian <foradian@gmail.com>
2011-09-23 07:31:41 +0000

    prevented exam maximum_mark=minimum_mark = 0
    git-svn-id: file:///svn/fedena2.1/trunk@779 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -29,6 +29,7 @@ class Exam < ActiveRecord::Base
 
   validates_presence_of :maximum_marks
   validates_presence_of :minimum_marks
+  validates_numericality_of :minimum_marks, :maximum_marks, :greater_than => 0
 
   has_many :exam_scores
   has_many :archived_exam_scores
